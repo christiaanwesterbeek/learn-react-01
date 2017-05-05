a0b4f9216b70ea2728fd742efbd7012ea0258685
Christiaan Westerbeek <chris@devotis.nl>
2017-05-05 14:48:44 +0200

    Done with 13rd video

---
+++ src/NewProps.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class NewProps extends Component {
+  constructor() {
+    super()
+    this.state = {
+      increasing: false
+    }
+  }
+
+  update() {
+    ReactDOM.render(
+      <NewProps val={this.props.val + 1} />,
+      document.getElementById('root')
+    )
+
+  }
+  componentWillMount() {
+    console.log('componentWillMount')
+  }
+  componentWillReceiveProps(nextProps){
+    console.log('componentWillReceiveProps', nextProps)
+    this.setState({
+      increasing: nextProps.val > this.props.val
+    })
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.val % 5 === 0
+  }
+  render() {
+    console.log('render', this.state.increasing)
+    return (
+      <button onClick={this.update.bind(this)}>
+        props.val: {this.props.val} |
+        state.increasing: {this.state.increasing}
+      </button>
+    )
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log(`prevProps: ${prevProps.val}`)
+  }
+  componentDidMount() {
+    console.log('componentDidMount', ReactDOM.findDOMNode(this))
+  }
+  componentWillUnmount() {
+    console.log('componentUnmount')
+  }
+}
+
+NewProps.defaultProps = {
+  val: 0
+}
+
+export default NewProps
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import LifeCycle from './LifeCycle'
+import NewProps from './NewProps'
 
 ReactDOM.render(
-  <LifeCycle />,
+  <NewProps />,
   document.getElementById('root')
 )
