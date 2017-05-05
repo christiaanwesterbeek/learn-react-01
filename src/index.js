aa08648e5acaa6518c41102163f5b7e4d2521a09
Christiaan Westerbeek <chris@devotis.nl>
2017-05-05 16:15:10 +0200

    Done with 15th video

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import UseMap from './UseMap'
+import HOC from './HOC'
 
 ReactDOM.render(
-  <UseMap />,
+  <HOC />,
   document.getElementById('root')
 )
