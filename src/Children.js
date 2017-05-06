d0c9a4423fd462ef5fdc4c2bc28a5c170024eff7
Christiaan Westerbeek <chris@devotis.nl>
2017-05-06 08:08:54 +0200

    Done with 18th video

---
+++ src/Children.js
@@ -0,0 +1,27 @@
+import React, { Component} from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <Parent>
+        <div className="childA"></div>
+        <div className="childB"></div>
+      </Parent>
+    )
+  }
+
+}
+
+class Parent extends Component {
+  render() {
+    let items = React.Children.map(this.props.children, child => child)
+    // let items = React.Children.toArray(this.props.children)
+    // let items = React.Children
+    //   .forEach(this.props.children, child => console.log(child.props.className))
+    // let items = React.Children.only(this.props.children)
+    console.log(items)
+    return null
+  }
+}
+
+export default App
